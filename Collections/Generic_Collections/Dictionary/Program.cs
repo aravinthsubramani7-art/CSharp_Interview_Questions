8ee19b4bea80293b7b7b1db9e446592483642169
Aravinth-DotNetDeveloper <aravinthsubramani7@gmail.com>
2026-05-28 02:07:29 +0000

    Dictionary & GenericCollectionWithUserDefinedTypes - changes

---
+++ Collections/Generic_Collections/Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace GenericCollections
+{
+    public class DictionaryCollection
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, object> dt = new Dictionary<string, object>();
+            dt.Add("EmpId", 1001);
+            dt.Add("EmpName", "John Doe");
+            dt.Add("EmpDept", "IT");
+            dt.Add("EmpSalary", 50000);
+            dt.Add("EmpEmail", "john.doe@company.com");
+            dt.Add("EmpPhone", "123-456-7890");
+
+            //the same Keys property available for Dictionary collection as well, but the order of keys is in sequential order where we added the key values, unlike hashtable where the order of keys is not guaranteed
+            foreach(string key in dt.Keys)
+                Console.WriteLine(key);
+            
+            Console.WriteLine();
+            foreach(string key in dt.Keys)
+                Console.WriteLine(key + ": " + dt[key]);
+        }
+    }
+}
---
+++ Collections/Generic_Collections/GenericCollectionWithUserDefinedTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CollectionProject
+{
+    public class Customer
+    {
+        public int cusId { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public double balance { get; set; }
+    }
+
+    public class TestCustomer
+    {
+        public static void Main(string[] args)
+        {
+            List<Customer> customer = new List<Customer>();
+            Customer c1 = new Customer { cusId = 1001, name = "John Doe", city = "New York", balance = 5000 };
+            Customer c2 = new Customer { cusId = 1002, name = "Jane Smith", city = "Los Angeles", balance = 3000 };
+            Customer c3 = new Customer { cusId = 1003, name = "Bob Johnson", city = "Chicago", balance = 7000 };   
+            Customer c4 = new Customer { cusId = 1004, name = "Alice Williams", city = "Houston", balance = 2000 };
+
+            customer.Add(c1);
+            customer.Add(c2);
+            customer.Add(c3);  
+            customer.Add(c4);
+
+            //instead of adding customer object into the List cllection, we have a way to add the objects at a time --> AddRange() method, which is used to add a collection of objects into the List collection, we can pass the List collection as a parameter to the AddRange() method, and it will add all the objects in the List collection into the List collection
+
+            foreach(Customer obj in customer)
+                Console.WriteLine(obj.cusId + " " + obj.name + " " + obj.city + " " + obj.balance);
+
+            Console.ReadLine();
+        }
+    }
+}
