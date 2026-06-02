6d1710fd9f3b097eac1cac175f57f021fda2e459
Aravinth-DotNetDeveloper <aravinthsubramani7@gmail.com>
2026-06-02 17:38:59 +0000

    Recursion - changes

---
+++ Recursion/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RecursiveProjects
+{
+    public class Program
+    {
+        public static int Factorial(int n)
+        {
+            if(n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Enter a number to find out Factorial: ");
+            int number = int.Parse(Console.ReadLine());
+            int result = Factorial(number);
+            Console.WriteLine($"Factorial of {number} is: {result}");
+            Console.ReadLine();
+        }
+    }
+}
