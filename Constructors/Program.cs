d5264c877170c1a00c1c67b4ba9a09a35fa99f8f
Aravinth-DotNetDeveloper <aravinthsubramani7@gmail.com>
2026-06-09 00:39:05 +0000

    Constructor - changes

---
+++ Constructors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace ConstrcutorProjects
+{
+    // public class Program
+    // {
+    //     int i;
+    //     bool b;
+    //     public static void Main(string[] args)
+    //     {
+    //         Program p = new Program();
+    //         Console.WriteLine("value of i: " + p.i); //0 through the implicit constructor the i is initialized with 0   
+    //         Console.WriteLine("value of b: " + p.b); //false
+    //     }
+    // }
+
+    //--------------------------------Explicit constructor--------------------------------
+    public class ExplicitContructorDemo
+    {
+        public ExplicitContructorDemo() //this is the explicit constructor we defined, defining can be implicit or explicit, but calling a constructor should be explicit only
+        {
+            Console.WriteLine("Constructor is called");
+        }
+
+        static void Main()
+        {
+            ExplicitContructorDemo explicitContructorDemo = new ExplicitContructorDemo();
+            ExplicitContructorDemo explicitContructorDemo1 = new ExplicitContructorDemo();
+            ExplicitContructorDemo explicitContructorDemo2 = new ExplicitContructorDemo();
+
+            //every time you create a instnace of the class, the constructor will be called.
+            Console.ReadLine();
+        }
+    }
+}
